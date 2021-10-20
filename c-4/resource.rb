506daa18a731b1a64dff74afb63008ec17cf1ff1
isaka1022 <isaka1022@gmail.com>
2021-10-20 22:10:52 +0900

    ファイル関係の責務を集約する

--- c-4/resource.rb
+++ c-4/resource.rb
@@ -1,30 +1,18 @@
 # 悪い例
 
-def read_customer
-  @cusotomer_file = File.open(@name + ".rec" + "r+")
-  @balance = BigDecimal(@customer_file.gets)
+def read_customer(file)
+  @balance = BigDecimal(file.gets)
 end
 
-def write_customer
+def write_customer(file)
   @customer_file.rewind
   @customer_file.puts @balance.to_s
-  @customer_file.close
 end
 
 def update_customer(transaction_amount)
-  read_customer
+  file = file.open(@name + '.rec', 'r+')
+  read_customer(file)
   @balance = @balance.add(transaction_amount, 2)
-  write_customer
-end
-
-# customer_fileというインスタンス変数により緊密に結合
-
-# 残高の更新を更新後の値が負でない場合にのみ行うようにする
-# オープンしているファイルの数が多すぎる：wirte_customerが呼ばれないのでファイルがクローズされない
-def update_customer(transaction_amount)
-  read_customer
-  if (transaction_amount >= 0.00)
-    @balance = @balance.add(transaction_amount, 2)
-    write_customer
-  end
+  write_customer(file)
+  file.close
 end
