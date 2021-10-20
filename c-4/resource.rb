0b21237870035cc92083596512ef620e56fc0e7d
isaka1022 <isaka1022@gmail.com>
2021-10-20 22:12:35 +0900

    ブロックを使う

--- c-4/resource.rb
+++ c-4/resource.rb
@@ -9,10 +9,12 @@ def write_customer(file)
   @customer_file.puts @balance.to_s
 end
 
+# 始めた場所で作業が終わる
+# ブロックの終端でファイルがクローズされる
 def update_customer(transaction_amount)
-  file = file.open(@name + '.rec', 'r+')
-  read_customer(file)
-  @balance = @balance.add(transaction_amount, 2)
-  write_customer(file)
-  file.close
+  File.open(@name + '.rec', 'r+') do |file|
+    read_customer(file)
+    @balance = @balance.add(transaction_amount, 2)
+    write_customer(file)
+  end
 end
