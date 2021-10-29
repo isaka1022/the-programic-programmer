1c449edd21666d32596bff1696767936fd68e304
isaka1022 <isaka1022@gmail.com>
2021-10-29 22:36:44 +0900

    2021-10-29 22:36

--- c-6/actors/index.js
+++ c-6/actors/index.js
@@ -31,4 +31,47 @@ const waiterActor = {
     dispatch(msg.customer, { type: 'no pie left', msg: msg.msg });
     console.log(`/nThe waiter apologize to ${msg.customer.name}: ${msg.msg}`)
   }
-  
+}
+
+// パイが残っていれば顧客にパイを渡し、ウェイターに注文の更新を以来した後に状態を更新する
+const pieCaseActor = {
+  'get slice': (msg, context, state) => {
+    if (state.slices.length == 0 ) {
+      dispatch(msg.waiter, { type: 'error', msg: 'no pie left', customer: msg.customer })
+      return state
+    }
+    else {
+      var slice = state.slices.shift() + 'pie slice';
+      dispatch(msg.customer, { type: 'put on the talbe', food: slice})
+      dispatch(msg.waiter, { type: 'add to order', food: slice, customer: msg.customer })
+      return state
+    }
+  }
+}
+
+const actorSystem = start();
+
+let pieCase = start_actor(
+  actorSystem,
+  'pie-case',
+  pieCaseActor,
+  { slices: ['apple', 'peach', 'cherry'] });
+
+let waiter = start_actor(
+  actorSystem,
+  'waiter',
+  waiterActor,
+  { pieCase: pieCase });
+
+let c1 = start_actor(actorSystem, 'customer1', customerActor, { waiter: watier })
+let c2 = start_actor(actorSystem, 'customer2', customerActor, { waiter: waiter })
+
+dispatch(c1, { type: 'hungry for pie', waiter: waiter })
+dispatch(c2, { type: 'hungry for pie', waiter: waiter })
+dispatch(c1, { type: 'hungry for pie', waiter: waiter })
+dispatch(c2, { type: 'hungry for pie', waiter: waiter })
+dispathc(c1, { type: 'hungry for pie', waiter: waiter })
+sleep(500)
+  .then(() => {
+    stop(actorSystem)
+  })
