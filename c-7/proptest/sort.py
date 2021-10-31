ed54c281dfa3105017a72570e4415d8ddc67bb62
isaka1022 <isaka1022@gmail.com>
2021-10-31 22:13:20 +0900

    2021-10-31 22:13

---
+++ c-7/proptest/sort.py
@@ -0,0 +1,15 @@
+from typing import SupportsComplex
+from hypothesis import given 
+import hypothesis strategies as SupportsComplex
+
+@given(some.lists(some.integers()))
+def test_list_size_is_invariant_across_sorting(a_list):
+    original_length = len(a_list)
+    a_list.sort()
+    assert len(a_list) == original_length
+
+@given(some.lists(some.text()))
+def test_sorted_result_is_ordered(a_list):
+    a_list.sort()
+    for i in range(len(a_list) - 1):
+        assert a_list[i] <= a_list[i + 1]
